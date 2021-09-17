8c15c63d9722b4895e7696b7a3f5ff51fad75a73
Vega Deftwing <johnathan698@gmail.com>
2021-09-16 21:28:31 -0500

    Delta panel finised, fixed build

--- src/Delta.cpp
+++ src/Delta.cpp
@@ -56,10 +56,33 @@ struct Delta : Module {
 		configParam(LMRGATEHOLD_PARAM, 0.f, 1.f, 0.f, "");
 	}
 
+	float R1T = 0.f;
+	float R2T = 0.f;
+	float R3T = 0.f;
+
 	void process(const ProcessArgs& args) override {
-		//BLOCK1 = (((R1*R1A+R1O) * (R2*R2A+R2)) * (R3*R3A+R3O))
-		//STAGE1OUTL = LEFT_INPUT * -BLOCK1
-		//STAGE1OUTR = RIGHT_INPUT * BLOCK1
+		//BLOCK 1
+		if (inputs[R1_INPUT].isConnected()){
+			R1T = inputs[R1_INPUT].getVoltage() * params[R1A_PARAM].getValue() + params[R1O_PARAM].getValue();
+		} else{
+			R1T = params[R1O_PARAM].getValue();
+		}
+		
+		if (inputs[R2_INPUT].isConnected()){
+			R2T = inputs[R2_INPUT].getVoltage() * params[R2A_PARAM].getValue() + params[R2O_PARAM].getValue();
+		} else{
+			R2T = params[R2O_PARAM].getValue();
+		}
+		
+		if (inputs[R3_INPUT].isConnected()){
+			R3T = inputs[R3_INPUT].getVoltage() * params[R3A_PARAM].getValue() + params[R3O_PARAM].getValue();
+		} else{
+			R3T = params[R3O_PARAM].getValue();
+		}
+
+		float BLOCK1 = R1T * R2T * R3T;
+		float STAGE1OUTL = inputs[LEFT_INPUT].getVoltage() * -BLOCK1;
+		float STAGE1OUTR = inputs[RIGHT_INPUT].getVoltage() * BLOCK1;
 
 		//BLOCK2 - clocksel or phase sel CS1,CS2,CS3 with xfade
 		//STAGE2OUTL = STAGE1OUTL * BLOCK2
@@ -68,8 +91,9 @@ struct Delta : Module {
 		//BLOCK3 - gate'd X-MOD of channels
 		// if(RMLGATE){ STAGE3OUTL = STAGE2OUTL * STAGE2OUTR } else{ STAGE3OUTL = STAGE2OUTL }
 		// if(LMRGATE){ STAGE3OUTR = STAGE2OUTR * STAGE2OUTL } else{ STAGE3OUTR = STAGE2OUTR }
-		outputs[LEFT_OUTPUT].setVoltage(inputs[LEFT_INPUT].getVoltage());
-		outputs[RIGHT_OUTPUT].setVoltage(inputs[RIGHT_INPUT].getVoltage());
+
+		outputs[LEFT_OUTPUT].setVoltage(STAGE1OUTL);
+		outputs[RIGHT_OUTPUT].setVoltage(STAGE1OUTR);
 	}
 };
 
@@ -79,20 +103,21 @@ struct DeltaWidget : ModuleWidget {
 		setModule(module);
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/delta1.svg")));
 
-		addChild(createWidget<Bolt>(Vec(RACK_GRID_WIDTH*6, + 5)));
-		addChild(createWidget<Bolt>(Vec(box.size.x - 2 * RACK_GRID_WIDTH + 5, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<Bolt>(Vec(RACK_GRID_WIDTH - 13, + 5)));
+		addChild(createWidget<Bolt>(Vec(78, 5)));
+		addChild(createWidget<Bolt>(Vec(box.size.x - 2 * RACK_GRID_WIDTH + 14.5, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(15.24, 27.932)), module, Delta::R1A_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(25.4, 27.932)), module, Delta::R1O_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(15.24, 38.092)), module, Delta::R2A_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(25.4, 38.092)), module, Delta::R2O_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(15.24, 48.252)), module, Delta::R3A_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(25.4, 48.252)), module, Delta::R3O_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(25.4, 63.413)), module, Delta::MANUALCLOCK_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(15.24, 88.732)), module, Delta::RMLGATEMANUAL_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(25.4, 88.732)), module, Delta::RMLGATEHOLD_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(15.24, 98.892)), module, Delta::LMRGATEMANUAL_PARAM));
-		addParam(createParamCentered<HexKnob>(mm2px(Vec(25.4, 98.892)), module, Delta::LMRGATEHOLD_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(15.24, 27.932)), module, Delta::R1A_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 27.932)), module, Delta::R1O_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(15.24, 38.092)), module, Delta::R2A_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 38.092)), module, Delta::R2O_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(15.24, 48.252)), module, Delta::R3A_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 48.252)), module, Delta::R3O_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 63.413)), module, Delta::MANUALCLOCK_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(15.24, 88.732)), module, Delta::RMLGATEMANUAL_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 88.732)), module, Delta::RMLGATEHOLD_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(15.24, 98.892)), module, Delta::LMRGATEMANUAL_PARAM));
+		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 98.892)), module, Delta::LMRGATEHOLD_PARAM));
 
 		addInput(createInputCentered<InJack>(mm2px(Vec(10.16, 7.08)), module, Delta::LEFT_INPUT));
 		addInput(createInputCentered<InJack>(mm2px(Vec(20.312, 7.08)), module, Delta::RIGHT_INPUT));
@@ -106,8 +131,8 @@ struct DeltaWidget : ModuleWidget {
 		addInput(createInputCentered<InJack>(mm2px(Vec(5.08, 88.732)), module, Delta::RMLGATE_INPUT));
 		addInput(createInputCentered<InJack>(mm2px(Vec(5.08, 98.892)), module, Delta::LMRGATE_INPUT));
 
-		addOutput(createOutputCentered<OutJack>(mm2px(Vec(14.795, 123.205)), module, Delta::LEFT_OUTPUT));
-		addOutput(createOutputCentered<OutJack>(mm2px(Vec(24.947, 123.205)), module, Delta::RIGHT_OUTPUT));
+		addOutput(createOutputCentered<OutJack>(mm2px(Vec(14.795, 119.205)), module, Delta::LEFT_OUTPUT));
+		addOutput(createOutputCentered<OutJack>(mm2px(Vec(24.947, 119.205)), module, Delta::RIGHT_OUTPUT));
 
 		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(5.08, 63.413)), module, Delta::STAGE_LIGHT));
 		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(20.25, 88.732)), module, Delta::LMR_LIGHT));
