e2a458f0a854206d8e2ccaea62909cea8f7643e1
Vega Deftwing <johnathan698@gmail.com>
2021-09-17 10:46:46 -0500

    Started Modes, Anger w/ just sustain is partially working now

--- src/Delta.cpp
+++ src/Delta.cpp
@@ -43,12 +43,12 @@ struct Delta : Module {
 
 	Delta() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(R1A_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(R1O_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(R2A_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(R2O_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(R3A_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(R3O_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(R1A_PARAM, 0.f, 1.f, 0.f, "Near Stage Attenuation");
+		configParam(R1O_PARAM, 0.f, 1.f, 0.f, "Near Stage Offset");
+		configParam(R2A_PARAM, 0.f, 1.f, 0.f, "Mid Stage Attenuation");
+		configParam(R2O_PARAM, 0.f, 1.f, 0.f, "Mid Stage Offset");
+		configParam(R3A_PARAM, 0.f, 1.f, 0.f, "Far Stage Attenuation");
+		configParam(R3O_PARAM, 0.f, 1.f, 0.f, "Far Stage Offset");
 		configParam(MANUALCLOCK_PARAM, 0.f, 1.f, 0.f, "");
 		configParam(RMLGATEMANUAL_PARAM, 0.f, 1.f, 0.f, "");
 		configParam(RMLGATEHOLD_PARAM, 0.f, 1.f, 0.f, "");
@@ -113,11 +113,11 @@ struct DeltaWidget : ModuleWidget {
 		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 38.092)), module, Delta::R2O_PARAM));
 		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(15.24, 48.252)), module, Delta::R3A_PARAM));
 		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 48.252)), module, Delta::R3O_PARAM));
-		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 63.413)), module, Delta::MANUALCLOCK_PARAM));
-		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(15.24, 88.732)), module, Delta::RMLGATEMANUAL_PARAM));
-		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 88.732)), module, Delta::RMLGATEHOLD_PARAM));
-		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(15.24, 98.892)), module, Delta::LMRGATEMANUAL_PARAM));
-		addParam(createParamCentered<MedHexKnob>(mm2px(Vec(25.4, 98.892)), module, Delta::LMRGATEHOLD_PARAM));
+		addParam(createParamCentered<TL1105>(mm2px(Vec(25.4, 63.413)), module, Delta::MANUALCLOCK_PARAM));
+		addParam(createParamCentered<TL1105>(mm2px(Vec(15.24, 88.732)), module, Delta::RMLGATEMANUAL_PARAM));
+		addParam(createParamCentered<TL1105>(mm2px(Vec(25.4, 88.732)), module, Delta::RMLGATEHOLD_PARAM));
+		addParam(createParamCentered<TL1105>(mm2px(Vec(15.24, 98.892)), module, Delta::LMRGATEMANUAL_PARAM));
+		addParam(createParamCentered<TL1105>(mm2px(Vec(25.4, 98.892)), module, Delta::LMRGATEHOLD_PARAM));
 
 		addInput(createInputCentered<InJack>(mm2px(Vec(10.16, 7.08)), module, Delta::LEFT_INPUT));
 		addInput(createInputCentered<InJack>(mm2px(Vec(20.312, 7.08)), module, Delta::RIGHT_INPUT));
